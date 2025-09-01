3c765fcfd6956979790cc9215e50481e14bdae47
Jfdeev <jfrsoares161205@gmail.com>
2025-09-01 19:20:38 -0300

    feat; create a first search with agent

--- main.py
+++ main.py
@@ -8,10 +8,29 @@ from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-tools = [TavilySearch]
+tools = [TavilySearch()]
+llm = ChatGoogleGenerativeAI(
+    temperature=0.7,
+    model="gemini-1.5-flash",
+)
+# Pega um prompt de reação do hub do langchain, o hub serve para compartilhar projetos entre a comunidade
+react_prompt = hub.pull("hwchase17/react")
+# Mecanismo de raciocinio seguindo as normas do Agente de Reação
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
 
 def main():
-    print("Hello from search-ai-agent!")
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for ai engineers using langchain on linkedin and list their details"
+        }
+    )
+    print(result)
 
 
 if __name__ == "__main__":
