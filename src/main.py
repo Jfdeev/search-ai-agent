51273caac96eab22a7e6e75d0051f75f8b7ad1b4
Jfdeev <jfrsoares161205@gmail.com>
2025-09-02 19:36:00 -0300

    feat: implements LLC to format outputs

--- src/main.py
+++ src/main.py
@@ -35,7 +35,9 @@ agent = create_react_agent(
     prompt=react_prompt_with_validation,
     )
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
 
 
 def main():
